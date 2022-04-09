7316922623c8ca77d2d1570475bdba1de1c48bb4
pr_Mais <mais.alheraki@gmail.com>
2022-04-09 06:47:28 +0300

    2️⃣ second step: reading documents

---
+++ firestore_type_safety_with_converter/1_read_documents_without_type_safety/instructions.md
@@ -0,0 +1,79 @@
+# Read Firestore documents without type-safety
+
+> 💡 if you get stuck, click on **show solution** button below then run the code to see the final result in this step.
+
+In this step, we will read a Firestore **collection** named `poll`, which contains a list of Polls.
+
+## Create `poll` collection in Firestore
+
+At this stage, we don't have such a collection on our Firebase project yet, so let's go and create it with some dummy data. 
+
+On the Firebase console, navigate to Firestore. Click on **"Create database"**, then choose **"Test mode"**. 
+> ⚠️ The test mode will make our database open for reads and writes without any security rules, if you're working in a real project, make sure to switch to **production mode** once you're done testing.
+
+![Create Firestore database](http://localhost:8080/assets/create-firestore-database.gif)
+
+Next, create a new collection named `poll`. It will look like this:
+
+![Poll collection in Firestore](http://localhost:8080/assets/poll-collection.png)
+
+You can create as many documents as you like.
+
+## Reference the polls collection
+
+It's time to ask Firestore for the list of polls! 
+
+> You can see a number of new lines of code added for you in this step. As our focus is on effect querying and type-safety in this workshop, we won't write much UI code.
+
+Let's scroll down to `TODO(1)`, in line `233`. The first step in reading or writing to Firestore, is to have a reference to what collection we want.
+
+```dart
+CollectionReference<Map<String, dynamic>> get _pollsRef =>
+      _firestore.collection('poll');
+```
+
+## Listen to polls from the collection reference
+
+In `TODO(2)`, we will listen to all the documents in `_pollsRef`. The listener will be attached in the constructor of `PollsState`.
+
+```dart
+_pollsRef.snapshots().listen((event) {
+  _polls = event.docs;
+  notifyListeners();
+});
+```
+
+## Display the polls
+
+In `TODO(3)`, we will finally see the polls and answers in a `ListView`.
+
+```dart
+Expanded(
+  child: Consumer<PollsState>(
+    builder: (_, pollsState, __) => ListView.builder(
+      padding: const EdgeInsets.all(30),
+      itemCount: pollsState.polls.length,
+      itemBuilder: (_, index) {
+        final pollDoc = pollsState.polls[index];
+
+        return PollListItem(
+          poll: pollDoc,
+          onVote: (i) {},
+        );
+      },
+    ),
+  ),
+)
+```
+
+## What's the problem with using `Map<String, dynamic>` as the reference type
+
+The type assigned to the collection reference is `CollectionReference<Map<String, dynamic>>`, this means the data inside each document that we will get from this reference will be of type `Map<String, dynamic>`. For example, to get the question text of a single poll:
+
+```dart
+final question = polls[0]?.data()?['question'];
+```
+
+The type of `question` isn't known. Firestore is NoSQL, each field could have several types, so `question` could be a `null` in one document, and a `int` in another document, we would never be able to set any type rules from the database side! 
+
+This requires us to code defensively. Type-safety is a very crucial part of using a NoSQL database in our projects.
---
+++ firestore_type_safety_with_converter/1_read_documents_without_type_safety/snippet.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+const firebaseOptions = FirebaseOptions(
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: firebaseOptions);
+
+  runApp(
+    ChangeNotifierProvider<AuthState>(
+      create: (_) => AuthState(),
+      child: const FirestoreApp(),
+    ),
+  );
+}
+
+class FirestoreApp extends StatefulWidget {
+  const FirestoreApp({Key? key}) : super(key: key);
+
+  @override
+  State<FirestoreApp> createState() => _FirestoreAppState();
+}
+
+class _FirestoreAppState extends State<FirestoreApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: Consumer<AuthState>(
+        builder: (_, auth, __) {
+          // If a guest has signed in.
+          if (auth._user != null) {
+            return ChangeNotifierProvider<PollsState>(
+              create: (_) => PollsState(),
+              child: const PollsPage(),
+            );
+          } else {
+            // If no guest signed in, we prompt them to sign in.
+            return const WelcomePage();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  final nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Get Started with FlutterFire'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome to FlutterFire Polls!',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              const SizedBox(height: 20),
+              const Text('To get started, enter your name.'),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: nameController,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Consumer<AuthState>(
+                builder: (_, auth, __) => ElevatedButton(
+                  onPressed: () => auth.signUpNewGuest(nameController.text),
+                  child: const Text('Start'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PollsPage extends StatelessWidget {
+  const PollsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final name = context.watch<AuthState>().user!.displayName;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome ${name ?? 'User'}'),
+      ),
+      body: Column(
+        children: [
+          //TODO(3): consume the `PollsState` to get the list of current polls, then pass them to a `ListView` widget.
+        ],
+      ),
+    );
+  }
+}
+
+class PollListItem extends StatelessWidget {
+  const PollListItem({
+    Key? key,
+    required this.poll,
+    required this.onVote,
+  }) : super(key: key);
+
+  final QueryDocumentSnapshot<Map<String, dynamic>> poll;
+  final void Function(int) onVote;
+
+  @override
+  Widget build(BuildContext context) {
+    final uid = context.read<AuthState>().user!.uid;
+    final pollData = poll.data();
+    final answers = pollData['answers'];
+
+    /// Votes is the total of users who voted for this answer by its Id.
+    int votes(Map<String, dynamic> answer) {
+      int votes = 0;
+
+      for (String user in pollData['users'].keys) {
+        if (pollData['users'][user] == answer['id']) {
+          votes++;
+        }
+      }
+      return votes;
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            pollData['question'],
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        for (var i = 0; i < answers.length; i++)
+          ListTile(
+            title: Text(answers[i]['text']),
+            selected: pollData['users'][uid] == i,
+            trailing: Text('Votes: ${votes(answers[i])}'),
+            onTap: () => onVote(answers[i]['id']),
+          ),
+        const Divider(),
+      ],
+    );
+  }
+}
+
+class AuthState extends ChangeNotifier {
+  User? _user;
+  User? get user => _user;
+
+  final _auth = FirebaseAuth.instance;
+
+  AuthState() {
+    _auth.authStateChanges().listen((user) {
+      _user = user;
+      notifyListeners();
+    });
+  }
+
+  Future<void> signUpNewGuest(String name) async {
+    try {
+      await _auth.signInAnonymously();
+      await _auth.currentUser!.updateDisplayName(name);
+      await _auth.currentUser!.reload();
+
+      _user = FirebaseAuth.instance.currentUser;
+      notifyListeners();
+    } on FirebaseAuthException {
+      rethrow;
+    }
+  }
+}
+
+class PollsState extends ChangeNotifier {
+  List<QueryDocumentSnapshot<Map<String, dynamic>>>? _polls;
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> get polls => _polls ?? [];
+
+  final _firestore = FirebaseFirestore.instance;
+
+  //TODO(1): create a collection reference to the collection named `poll`.
+
+  PollsState() {
+    //TODO(2): listen to real-time changes in the polls collection using `snapshots`.
+  }
+}
